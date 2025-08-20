3714fffc19564c2d8c745e27834dd1dc0fc07084
nisaltnbas <altnbasnisa@gmail.com>
2025-08-20 09:12:39 +0300

    Static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
-// next.config.mjs
 const isProd = process.env.NODE_ENV === 'production';
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
